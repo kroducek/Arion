b212dd444a3080eaaf237c438e4b089d26b88dcf
kroducek <krodukcz@gmail.com>
2026-06-28 01:53:48 +0200

    Last Sync: 2026-06-28 01:53 (Mobile)

--- main_dnd.py
+++ main_dnd.py
@@ -71,7 +71,6 @@ DND_COGS = [
     
     # D&D logika / postavy
     "src.logic.profile",
-    "src.logic.profile_render",
     "src.logic.stats",
     "src.logic.combat",
     "src.logic.inventory",
