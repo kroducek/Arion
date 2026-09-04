38a43bdbd618b246a63d76d1c9c6e7ad1f7678b2
kroducek <krodukcz@gmail.com>
2026-09-04 16:23:58 +0200

    Last Sync: 2026-09-04 16:23 (Mobile)

--- src/core/bot/cards.py
+++ src/core/bot/cards.py
@@ -93,6 +93,9 @@ SEED_CARDS = [
     {"id": 16, "name": "Alice Aurelion",       "description": "Poslední čistá krev z královského rodu Aurionisu", "image": "alice-queen.png",               "collection": "queen"},
     {"id": 17, "name": "Noxarath",       "description": "Čarodějka smrti a bohyně temnoty.", "image": "noxarath.png",               "collection": "witches"},
     {"id": 18, "name": "Embra",       "description": "Čarodějka hněvu, jenž byla zostuzena", "image": "embra.png",               "collection": "witches"},
+    {"id": 19, "name": "Hádankář",       "description": "Jeho síla je nezměrná, možná největší na tomto světě…", "image": "hadankar2.png",               "collection": "jesters"},
+    {"id": 20, "name": "Hao",       "description": "Nejsilnější vyvolený a rank 1 dobrodruh.", "image": "hao.png",               "collection": "worthy"},
+    {"id": 21, "name": "Bojový šašek",       "description": "Šašek jenž vyhledává silné soupeře", "image": "bojovy_sasek.png",               "collection": "jesters"},
 ]
 
 # ---------------------------------------------------------------------------
