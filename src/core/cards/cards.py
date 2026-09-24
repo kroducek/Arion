b5eabbcf7e3ed0e988fb93305ae4951e4d272c44
kroducek <krodukcz@gmail.com>
2026-09-24 20:43:15 +0200

    Last Sync: 2026-09-24 20:43 (Mobile)

--- src/core/cards/cards.py
+++ src/core/cards/cards.py
@@ -514,6 +514,7 @@ SEED_CARDS = [
     {"id": 29, "name": "Levitující šašek",       "description": "Miluje chaos, často napodobuje emoce a výrazy ostatních", "image": "letajici_sasek.png",               "collection": "jesters"},
     {"id": 30, "name": "Remi",       "description": "Vyvolená služka jenž po smrti svého pána ztratila cestu", "image": "remi.png",               "collection": "chosen"},
     {"id": 31, "name": "Hao",       "description": "Nejsilnější vyvolený jenž okolo sebe shromažďuje silné jedince", "image": "hao2.png",               "collection": "chosen"},
+    {"id": 32, "name": "První stín",       "description": "Žije v něm jakýsi mimozemský symbiot..", "image": "prvni_stin.png",               "collection": "shadows"},
 
 ]
 
